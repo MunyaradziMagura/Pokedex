48debcb2c9bf2ceda14e08171bb8e45790462fa1
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-09-23 13:39:38 +0930

    application layout outline

--- pokedex/src/render.js
+++ pokedex/src/render.js
@@ -1,1 +1,23 @@
 console.log("hello world")
+
+// pokemon colour types
+const typeColors = {
+    "rock":     [182, 158,  49],
+    "ghost":    [112,  85, 155],
+    "steel":    [183, 185, 208],
+    "water":    [100, 147, 235],
+    "grass":    [116, 203,  72],
+    "psychic":  [251,  85, 132],
+    "ice":      [154, 214, 223],
+    "dark":     [117,  87,  76],
+    "fairy":    [230, 158, 172],
+    "normal":   [170, 166, 127],
+    "fighting": [193,  34,  57],
+    "flying":   [168, 145, 236],
+    "poison":   [164,  62, 158],
+    "ground":   [222, 193, 107],
+    "bug":      [167, 183,  35],
+    "fire":     [245, 125,  49],
+    "electric": [249, 207,  48],
+    "dragon":   [112,  55, 255]
+}
